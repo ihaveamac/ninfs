1e3e09e05ae730d7931ce865e7317d4d180634c7
Ian Burgwin <ian@ianburgwin.net>
2018-03-01 22:37:42 -0800

    make fuse3ds executable with `<python-cmd> -mfuse3ds <mount-type>`

---
+++ fuse3ds/__main__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import importlib
+import sys
+
+mount_types = ('cci', 'cdn', 'cia', 'nand', 'ncch', 'romfs', 'sd')
+
+def print_types():
+    print('Please provide a mount type as the first argument.')
+    print(' ', ', '.join(mount_types))
+
+if len(sys.argv) < 2:
+    print_types()
+    sys.exit(1)
+
+mount_type = sys.argv[1].lower()
+if mount_type not in mount_types:
+    print_types()
+    sys.exit(1)
+
+module = importlib.import_module('fuse3ds.mount_' + mount_type)
+del sys.argv[1]
+sys.exit(module.main())
