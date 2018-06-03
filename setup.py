bd4248d7eae2d769337cf9b8009572593624b223
Ian Burgwin <ian@ianburgwin.net>
2018-06-02 18:41:54 -0700

    version 1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='fuse-3ds',
-    version='1.0b9',
+    version='1.0',
     packages=['fuse3ds', 'fuse3ds.pyctr', 'fuse3ds.pyctr.types', 'fuse3ds.mount'],
     url='https://github.com/ihaveamac/fuse-3ds',
     license='MIT',
