a8617339dd3190f1ca90d8a3f051b288ee0a74f9
Ian Burgwin <ian@ianburgwin.net>
2018-08-30 13:56:41 -0700

    add license header to project files

--- fuse3ds/__main__.py
+++ fuse3ds/__main__.py
@@ -1,3 +1,9 @@
+# This file is a part of fuse-3ds.
+#
+# Copyright (c) 2017-2018 Ian Burgwin
+# This file is licensed under The MIT License (MIT).
+# You can find the full license text in LICENSE.md in the root of this project.
+
 from os.path import dirname, realpath
 from sys import argv, exit, path
 
--- fuse3ds/mount/nanddsi.py
+++ fuse3ds/mount/nanddsi.py
@@ -1,3 +1,9 @@
+# This file is a part of fuse-3ds.
+#
+# Copyright (c) 2017-2018 Ian Burgwin
+# This file is licensed under The MIT License (MIT).
+# You can find the full license text in LICENSE.md in the root of this project.
+
 """
 Mounts Nintendo DSi NAND images, creating a virtual filesystem of decrypted partitions.
 """
