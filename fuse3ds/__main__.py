ea9dc98f4af3eb0562d6f6d52b26f017d5b8cdc6
Ian Burgwin <ian@ianburgwin.net>
2018-11-29 19:08:48 -0800

    fix checking --install-desktop-entry happening before argv len check

--- fuse3ds/__main__.py
+++ fuse3ds/__main__.py
@@ -10,15 +10,14 @@ from sys import argv, exit, path
 # path fun times
 path.insert(0, dirname(realpath(__file__)))
 
+from main import exit_print_types, mount
+if len(argv) < 2:
+    exit_print_types()
+
 if argv[1] == '--install-desktop-entry':
     from main import create_desktop_entry
     prefix = None if len(argv) < 3 else argv[2]
     create_desktop_entry(prefix)
     exit(0)
-
-from main import exit_print_types, mount
-
-if len(argv) < 2:
-    exit_print_types()
 
 exit(mount(argv.pop(1).lower()))
