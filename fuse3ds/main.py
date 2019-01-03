7ac920b1326ef528e267759ca615affa300d2cc2
Ian Burgwin <ian@ianburgwin.net>
2019-01-02 17:42:20 -0800

    update license year

--- fuse3ds/main.py
+++ fuse3ds/main.py
@@ -2,7 +2,7 @@
 
 # This file is a part of fuse-3ds.
 #
-# Copyright (c) 2017-2018 Ian Burgwin
+# Copyright (c) 2017-2019 Ian Burgwin
 # This file is licensed under The MIT License (MIT).
 # You can find the full license text in LICENSE.md in the root of this project.
 
