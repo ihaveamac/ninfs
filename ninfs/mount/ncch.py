2d131629cdcb1cc1ca1f4f8a0331358ebf57567d
Ian Burgwin <ian@ianburgwin.net>
2019-04-17 17:46:57 -0700

    only read .code if decompression is required

--- ninfs/mount/ncch.py
+++ ninfs/mount/ncch.py
@@ -136,7 +136,7 @@ class NCCHContainerMount(LoggingMixIn, Operations):
         if _setup_romfs:
             self.setup_romfs()
 
-        if self.exefs_fuse and '/code.bin' in self.exefs_fuse.files:
+        if self.exefs_fuse and '/code.bin' in self.exefs_fuse.files and self.exefs_fuse.decompress_code:
             print('ExeFS: Reading .code...')
             data = self.exefs_fuse.read('/code.bin', self.exefs_fuse.files['/code.bin'].size, 0, 0)
             Thread(target=self.exefs_fuse.init, daemon=True, args=(path, data)).start()
