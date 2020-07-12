068687bff3f2ed04fa2abc673f53d3416d4c4f15
Ian Burgwin <ian@ianburgwin.net>
2020-07-11 17:15:24 -0700

    setup: update to pyctr 0.4.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    install_requires=['pycryptodomex', 'pyctr==0.3.1'],
+    install_requires=['pycryptodomex', 'pyctr==0.4.1'],
     # fusepy should be added here once the main repo has a new release with Windows support.
     entry_points={'console_scripts': ['ninfs = ninfs.main:gui',
                                       # not putting in gui_scripts since the cmd window is required and trying to
