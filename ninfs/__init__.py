d1b8d3036bde804c684af230fb5d713121ec854d
Ian Burgwin <ian@ianburgwin.net>
2020-08-04 10:49:26 -0700

    update copyright year

--- ninfs/__init__.py
+++ ninfs/__init__.py
@@ -1,10 +1,10 @@
 # This file is a part of ninfs.
 #
-# Copyright (c) 2017-2019 Ian Burgwin
+# Copyright (c) 2017-2020 Ian Burgwin
 # This file is licensed under The MIT License (MIT).
 # You can find the full license text in LICENSE.md in the root of this project.
 
 __author__    = 'ihaveamac'
-__copyright__ = 'Copyright (c) 2017-2019 Ian Burgwin'
+__copyright__ = 'Copyright (c) 2017-2020 Ian Burgwin'
 __license__   = 'MIT'
 __version__   = '2.0b1.dev0'
