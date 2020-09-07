af9244fdceb66b7cc38ea9220fd743ed2a3bb85a
Ian Burgwin <ian@ianburgwin.net>
2020-09-06 22:14:50 -0700

    remove pyi-app.spec, BUILDING.md, scripts/

--- scripts/make-exe-win.bat
+++
@@ -1,2 +0,0 @@
-py -3.7-32 -m PyInstaller pyi-app.spec
-py -3.7-64 -m PyInstaller pyi-app.spec
--- scripts/make-wheels-win.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Error checking from: https://stackoverflow.com/questions/734598
-
-for %%A in (32,64) do (
-    for %%V in (3.6,3.7,3.8) do (
-        py -%%V-%%A setup.py bdist_wheel
-        if %errorlevel% neq 0 exit /b %errorlevel%
-    )
-)
