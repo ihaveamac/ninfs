04079ea3fc0ed6fc4b2ae4ab958ad4d75d4a492a
Ian Burgwin <ian@ianburgwin.net>
2020-09-06 21:38:04 -0700

    _frozen_main: update for cx_Freeze

--- ninfs/_frozen_main.py
+++ ninfs/_frozen_main.py
@@ -4,20 +4,13 @@
 # This file is licensed under The MIT License (MIT).
 # You can find the full license text in LICENSE.md in the root of this project.
 
-from sys import argv, exit, path
-from os.path import dirname, realpath
+from sys import argv, exit, path, executable
+from os.path import dirname, join
 
 
-# noinspection PyUnresolvedReferences,PyProtectedMember
-def _():
-    # lazy way to get PyInstaller to detect the libraries, since this won't run at runtime
-    import fmt_detect
-    import reg_shell
-    from mount import _common, cci, cdn, cia, exefs, nandctr, nandhac, nandtwl, ncch, romfs, sd, srl, threedsx, titledir
-    from pyctr.type import crypto, exefs, ncch, romfs, smdh, tmd, util
-
-
-path.insert(0, dirname(realpath(__file__)))
+# this assumes it's cx_Freeze doing it
+# feels like there's a better way, but...
+path.insert(0, join(dirname(executable), 'lib', 'ninfs'))
 
 if len(argv) < 2 or argv[1] in {'gui', 'gui_i_want_to_be_an_admin_pls'}:
     from gui import start_gui
