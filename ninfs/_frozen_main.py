f6283410aded7ee1920479f6261f3142efed4d14
Ian Burgwin <ian@ianburgwin.net>
2020-09-18 05:21:12 -0700

    _frozen_main: use main.gui for mount type checking

--- ninfs/_frozen_main.py
+++ ninfs/_frozen_main.py
@@ -4,17 +4,12 @@
 # This file is licensed under The MIT License (MIT).
 # You can find the full license text in LICENSE.md in the root of this project.
 
-from sys import argv, exit, path, executable
+from sys import path, executable
 from os.path import dirname, join
-
 
 # this assumes it's cx_Freeze doing it
 # feels like there's a better way, but...
 path.insert(0, join(dirname(executable), 'lib', 'ninfs'))
 
-if len(argv) < 2 or argv[1] in {'gui', 'gui_i_want_to_be_an_admin_pls'}:
-    from gui import start_gui
-    start_gui()
-else:
-    from main import mount
-    exit(mount(argv.pop(1).lower()))
+from main import gui
+gui()
