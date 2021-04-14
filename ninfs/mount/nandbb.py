d56e9a9b21111e10969c2e4f2f9dadacf702f083
Ian Burgwin <ian@ianburgwin.net>
2021-04-14 13:25:30 -0700

    add license header to nandbb files

--- ninfs/mount/nandbb.py
+++ ninfs/mount/nandbb.py
@@ -1,3 +1,9 @@
+# This file is a part of ninfs.
+#
+# Copyright (c) 2017-2021 Ian Burgwin
+# This file is licensed under The MIT License (MIT).
+# You can find the full license text in LICENSE.md in the root of this project.
+
 """
 Mounts iQue Player NAND images, creating a virtual filesystem of the files contained within.
 """
