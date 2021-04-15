5eaf1e5aa97c8a012b1231d535cd3ec5eb48d2fd
Ian Burgwin <ian@ianburgwin.net>
2021-04-14 23:33:08 -0700

    gui: remove unused import, remove unused typeinfo module

--- ninfs/gui/__init__.py
+++ ninfs/gui/__init__.py
@@ -21,7 +21,6 @@ from uuid import uuid4
 from .about import NinfsAbout
 from .confighandler import get_bool, set_bool
 from .settings import NinfsSettings
-from .typeinfo import mount_types, ctr_types, twl_types, hac_types, bb_types, uses_directory
 from .updatecheck import thread_update_check
 from .wizardcontainer import WizardContainer, WizardTypeSelector, WizardFailedMount
 
