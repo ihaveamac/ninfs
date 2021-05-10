f92c38feecb49a4acf345e8a7df8a1c5916d175a
Ian Burgwin <ian@ianburgwin.net>
2021-05-10 08:18:35 -0700

    add scripts/make-icons.sh

---
+++ scripts/make-icons.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+if [[ `uname -s` = Darwin ]]; then
+    mkdir build
+    rm -r build/ninfs.iconset > /dev/null
+    mkdir build/ninfs.iconset
+
+    cp ninfs/gui/data/16x16.png build/ninfs.iconset/icon_16x16.png
+    cp ninfs/gui/data/32x32.png build/ninfs.iconset/icon_16x16@2x.png
+    cp ninfs/gui/data/32x32.png build/ninfs.iconset/icon_32x32.png
+    cp ninfs/gui/data/64x64.png build/ninfs.iconset/icon_32x32@2x.png
+    cp ninfs/gui/data/128x128.png build/ninfs.iconset/icon_128x128.png
+    cp ninfs/gui/data/1024x1024.png build/ninfs.iconset/icon_512x512@2x.png
+
+    convert ninfs/gui/data/1024x1024.png -resize 256x256 build/256x256_gen.png
+    convert ninfs/gui/data/1024x1024.png -resize 512x512 build/512x512_gen.png
+    cp build/256x256_gen.png build/ninfs.iconset/icon_128x128@2x.png
+    cp build/256x256_gen.png build/ninfs.iconset/icon_256x256.png
+    cp build/512x512_gen.png build/ninfs.iconset/icon_256x256@2x.png
+    cp build/512x512_gen.png build/ninfs.iconset/icon_512x512.png
+
+    iconutil --convert icns --output build/AppIcon.icns build/ninfs.iconset
+fi
+
+cd ninfs/gui/data
+convert 1024x1024.png 128x128.png 64x64.png 32x32.png 16x16.png \
+          \( -clone 2 -resize 48x48 \) \
+          \( -clone 0 -resize 256x256 \) \
+          -delete 0 windows.ico
