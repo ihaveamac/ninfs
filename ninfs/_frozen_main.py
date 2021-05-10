db33ef2b62f862ead21c1f18e33e841c098b821f
Ian Burgwin <ian@ianburgwin.net>
2021-05-09 20:08:21 -0700

    _frozen_main: support pyinstaller again (by not modifying sys.path)

--- ninfs/_frozen_main.py
+++ ninfs/_frozen_main.py
@@ -4,12 +4,16 @@
 # This file is licensed under The MIT License (MIT).
 # You can find the full license text in LICENSE.md in the root of this project.
 
-from sys import path, executable
+import sys
 from os.path import dirname, join
 
-# this assumes it's cx_Freeze doing it
-# feels like there's a better way, but...
-path.insert(0, join(dirname(executable), 'lib', 'ninfs'))
+if getattr(sys, 'frozen', False):
+    if hasattr(sys, '_MEIPASS'):
+        # PyInstaller
+        pass
+    else:
+        # cx_Freeze probably
+        sys.path.insert(0, join(dirname(sys.executable), 'lib', 'ninfs'))
 
 from main import gui
 gui()
