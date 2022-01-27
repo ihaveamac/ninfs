79bebcd1edcc78165d403ce9fd369855d90251c6
Ian Burgwin <ian@ianburgwin.net>
2022-01-26 20:12:17 -0800

    scripts/make-dmg-mac: quote variables, replace backticks with $()

--- scripts/make-dmg-mac.sh
+++ scripts/make-dmg-mac.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-NV=`python3 -c 'import ninfs.__init__ as i; print(i.__version__)'`
+NV=$(python3 -c 'import ninfs.__init__ as i; print(i.__version__)')
 DMGDIR=build/dmg/ninfs-$NV
-rm -rf $DMGDIR
+rm -rf "$DMGDIR"
 
 set -e -u
 
-mkdir -p $DMGDIR
+mkdir -p "$DMGDIR"
 
-cp -rpc dist/ninfs.app $DMGDIR/ninfs.app
-ln -s /Applications $DMGDIR/Applications
-cp resources/MacGettingStarted.pdf $DMGDIR/Getting\ Started.pdf
+cp -rpc dist/ninfs.app "$DMGDIR/ninfs.app"
+ln -s /Applications "$DMGDIR/Applications"
+cp resources/MacGettingStarted.pdf "$DMGDIR/Getting Started.pdf"
 
-hdiutil create -format UDZO -srcfolder $DMGDIR -fs HFS+ dist/ninfs-$NV-macos.dmg -ov
+hdiutil create -format UDZO -srcfolder "$DMGDIR" -fs HFS+ "dist/ninfs-$NV-macos.dmg" -ov
