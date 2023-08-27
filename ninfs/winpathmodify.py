bd85b0f4d898a7bfea899eadfff6079660d53457
Ian Burgwin <ian@ianburgwin.net>
2023-08-26 21:25:44 -0700

    rewrite winpathmodify (now can list paths)

--- ninfs/winpathmodify.py
+++ ninfs/winpathmodify.py
@@ -20,50 +20,46 @@ def refresh_environment():
     print('SendMessageTimeoutW:', res)
 
 
-def add(path: str):
+def do(op: str, path: str):
     k = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Environment', 0, winreg.KEY_ALL_ACCESS)
-
     value, keytype = winreg.QueryValueEx(k, 'Path')
 
-    paths: list = value.strip(';').split(';')
-    if path not in paths:
-        paths.append(path)
-
-    winreg.SetValueEx(k, 'Path', 0, keytype, ';'.join(paths))
-
-    winreg.CloseKey(k)
-
-    refresh_environment()
-
-
-def remove(path: str):
-    k = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Environment', 0, winreg.KEY_ALL_ACCESS)
-
-    value, keytype = winreg.QueryValueEx(k, 'Path')
-
-    paths: list = value.strip(';').split(';')
-    try:
-        paths.remove(path)
-    except ValueError:
-        pass
-
-    winreg.SetValueEx(k, 'Path', 0, keytype, ';'.join(paths))
+    paths: 'list[str]' = value.strip(';').split(';')
+    refresh = False
+    if op == 'add':
+        if path not in paths:
+            paths.append(path)
+            refresh = True
+        else:
+            print('Already in Path, not adding')
+    elif op == 'remove':
+        try:
+            paths.remove(path)
+            refresh = True
+        except ValueError:
+            pass
+    elif op == 'list':
+        for path in paths:
+            print(path)
 
     winreg.CloseKey(k)
-
-    refresh_environment()
+    if refresh:
+        refresh_environment()
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(description=r'Modify Path inside HKCU\Environment')
-    parser.add_argument('oper', help='Operation (add, remove)')
-    parser.add_argument('path', help='Path to add or remove')
+    #parser.add_argument('-allusers', help='Modify HKLM (PATH for all users)')
+    opers = parser.add_mutually_exclusive_group(required=True)
+    opers.add_argument('-add', metavar='PATH', help='Add path')
+    opers.add_argument('-remove', metavar='PATH', help='Remove path')
+    opers.add_argument('-list', help='List paths (user environment only)', action='store_true')
 
     args = parser.parse_args()
 
-    if args.oper == 'add':
-        add(args.path)
-    elif args.oper == 'remove':
-        remove(args.path)
-
-
+    if args.add:
+        do('add', args.add)
+    elif args.remove:
+        do('remove', args.remove)
+    elif args.list:
+        do('list', '')
