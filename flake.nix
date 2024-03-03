824f90152c0068f1d626641d753647315d876a9c
ihaveahax <ian@ianburgwin.net>
2024-03-03 14:03:40 -0600

    use github:ihaveamac/pyctr flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    pyctr.url = "github:ihaveamac/pyctr/master";
+    pyctr.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils }: 
+  outputs = inputs@{ self, nixpkgs, flake-utils, pyctr }:
 
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
 
         packages = rec {
           haccrypto = pkgs.python3Packages.callPackage ./haccrypto.nix {};
-          ninfs = pkgs.python3Packages.callPackage ./ninfs.nix { haccrypto = haccrypto; };
+          ninfs = pkgs.python3Packages.callPackage ./ninfs.nix { haccrypto = haccrypto; pyctr = pyctr.packages.${system}.pyctr; };
           default = ninfs;
         };
       }
