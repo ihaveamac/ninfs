78461945f0edf1245b134cc46c5b09f93418d0ea
ihaveahax <ian@ianburgwin.net>
2025-01-18 13:53:27 -0600

    use pyctr flake in default.nix (so it can use the flake pinned one)

--- default.nix
+++ default.nix
@@ -1,9 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 
-rec {
+let
+  # just so i can use the same pinned version as the flake...
+  pyctr = (import (builtins.getFlake (builtins.toString ./.)).inputs.pyctr { inherit pkgs; }).pyctr;
+in rec {
   haccrypto = pkgs.python3Packages.callPackage ./nix/haccrypto.nix {};
   mfusepy = pkgs.python3Packages.callPackage ./nix/mfusepy.nix {};
-  ninfs = pkgs.python3Packages.callPackage ./package.nix { haccrypto = haccrypto; mfusepy = mfusepy; };
+  ninfs = pkgs.python3Packages.callPackage ./package.nix { inherit pyctr; haccrypto = haccrypto; mfusepy = mfusepy; };
   ninfsNoGUI = ninfs.override { withGUI = false; };
   #ninfsNoGUI = pkgs.python3Packages.callPackage ./ninfs.nix { haccrypto = haccrypto; mfusepy = mfusepy; withGUI = false; };
 }
