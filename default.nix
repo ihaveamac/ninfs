7227f6c9cbc2372b0a5c74dbd770d12f9fc59bb4
ihaveahax <ian@ianburgwin.net>
2025-01-19 20:29:19 -0600

    remove flake-utils, some other cleanup

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-let
+{
+  pkgs ? import <nixpkgs> {},
   # just so i can use the same pinned version as the flake...
-  pyctr = (import (builtins.getFlake (builtins.toString ./.)).inputs.pyctr { inherit pkgs; }).pyctr;
-in rec {
+  pyctr ? (import (builtins.getFlake (builtins.toString ./.)).inputs.pyctr { inherit pkgs; }).pyctr
+}:
+
+rec {
   haccrypto = pkgs.python3Packages.callPackage ./nix/haccrypto.nix {};
   mfusepy = pkgs.python3Packages.callPackage ./nix/mfusepy.nix {};
   ninfs = pkgs.python3Packages.callPackage ./package.nix { inherit pyctr; haccrypto = haccrypto; mfusepy = mfusepy; };
--- flake.nix
+++ flake.nix
@@ -5,22 +5,39 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     pyctr.url = "github:ihaveamac/pyctr/master";
     pyctr.inputs.nixpkgs.follows = "nixpkgs";
-    pyctr.inputs.flake-utils.follows = "flake-utils";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, pyctr }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in {
-
-        packages = rec {
-          haccrypto = pkgs.python3Packages.callPackage ./nix/haccrypto.nix {};
-          mfusepy = pkgs.python3Packages.callPackage ./nix/mfusepy.nix {};
-          ninfs = pkgs.python3Packages.callPackage ./package.nix { haccrypto = haccrypto; mfusepy = mfusepy; pyctr = pyctr.packages.${system}.pyctr; };
-          ninfsNoGUI = ninfs.override { withGUI = false; };
-          default = ninfs;
-        };
-      }
-    );
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      pyctr,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      legacyPackages = forAllSystems (
+        system:
+        (import ./default.nix {
+          pkgs = import nixpkgs { inherit system; };
+          pyctr = pyctr.packages.${system}.pyctr;
+        })
+        // {
+          default = self.legacyPackages.${system}.ninfs;
+        }
+      );
+      packages = forAllSystems (
+        system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
+      );
+    };
 }
