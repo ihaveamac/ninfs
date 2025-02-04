03a6ea50aa5760f5aa841322ea629ae6980aba81
ihaveahax <ian@ianburgwin.net>
2025-02-03 23:09:18 -0600

    default.nix: read from flake.lock to get pyctr repo instead of using getFlake

--- default.nix
+++ default.nix
@@ -1,13 +1,28 @@
 {
-  pkgs ? import <nixpkgs> {},
+  pkgs ? import <nixpkgs> { },
   # just so i can use the same pinned version as the flake...
-  pyctr ? (import (builtins.getFlake (builtins.toString ./.)).inputs.pyctr { inherit pkgs; }).pyctr
+  pyctr ? (
+    let
+      flakeLock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      pyctr-repo = import (builtins.fetchTarball (
+        with flakeLock.nodes.pyctr.locked;
+        {
+          url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        }
+      )) { inherit pkgs; };
+    in
+    pyctr-repo.pyctr
+  ),
 }:
 
 rec {
-  haccrypto = pkgs.python3Packages.callPackage ./nix/haccrypto.nix {};
-  mfusepy = pkgs.python3Packages.callPackage ./nix/mfusepy.nix {};
-  ninfs = pkgs.python3Packages.callPackage ./package.nix { inherit pyctr; haccrypto = haccrypto; mfusepy = mfusepy; };
+  haccrypto = pkgs.python3Packages.callPackage ./nix/haccrypto.nix { };
+  mfusepy = pkgs.python3Packages.callPackage ./nix/mfusepy.nix { };
+  ninfs = pkgs.python3Packages.callPackage ./package.nix {
+    inherit pyctr;
+    haccrypto = haccrypto;
+    mfusepy = mfusepy;
+  };
   ninfsNoGUI = ninfs.override { withGUI = false; };
   #ninfsNoGUI = pkgs.python3Packages.callPackage ./ninfs.nix { haccrypto = haccrypto; mfusepy = mfusepy; withGUI = false; };
 }
